55e4ad498e8d63d99e4ea3ee584fb8a01954c260
Taylor Kaser <kasertaylor@gmail.com>
2025-03-14 14:39:35 -0700

    built my own componenet and took notes that uses the searchbyIngredient service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CocktailComponent } from './components/cocktailName/cocktail.component';
+import { IngredientComponent } from './components/ingredient/ingredient.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CocktailComponent],
+  imports: [RouterOutlet, CocktailComponent, IngredientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
