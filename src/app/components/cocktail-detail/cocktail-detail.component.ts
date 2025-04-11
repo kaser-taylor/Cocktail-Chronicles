9fc4cc4a9c27f7e2e5376979af541dfc9d2eb2ef
Taylor Kaser <kasertaylor@gmail.com>
2025-04-11 16:31:38 -0700

    observable service set up

--- src/app/components/cocktail-detail/cocktail-detail.component.ts
+++ src/app/components/cocktail-detail/cocktail-detail.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 // imports the cocktail card service which pulls the recipe for the card
 import { CocktailCardService } from '../../services/cocktail-card.service';
 import { CommonModule } from '@angular/common';
+import { CocktailFacts } from '../../models/cocktailCard.model';
 
 // Tells angular the following class is a component and to use this info for it
 @Component({
@@ -24,37 +25,44 @@ export class CocktailDetailComponent {
     private cocktailcardservice: CocktailCardService,
   ) {}
 
+
+  cocktailFacts: CocktailFacts | null = null;
 // this is a life cycle hook in angualr that runs automagically when the component loads
   ngOnInit(): void {
-
-    // grabs the id from the url this.route refers to the injected dependency in the constructor, .snapshot is a property of the dependency that contains a static snapshot of the route at the time of the request .paramMap is an object that provides access to route paratmesters. they are defined in the route with a colon: an example is /cocktial/:id. finally the .get gets the parameters from paramMap with the given variable in the call in this case id
-    const cocktailId = this.route.snapshot.paramMap.get('id');
-
-    // an if statement saying if there is a cocktailId then we do this
-    if(cocktailId) {
-      // this calls our cocktail card servcice to get the cocktails by the id we snagged early and subsribes to the obervable of the service
-      this.cocktailcardservice.getCocktailDetailsById(cocktailId).subscribe(
-        {
-          // next means it happens if the api responds sucessfully and it uses the ternary operator to set cocktail = to the response and = to null if there are no drinks so the app doesn't crash
-          next: (response) => {this.cocktail = response.drinks ? response.drinks[0]: null;
-
-            // So basically if there is a response from the api it calls the get ingredients function
-            if (this.cocktail) {
-            this.getIngredientsList();
-            }
-          },
-          // error is what displays if the observable or api fails
-          error: (error) => {console.error('Error fetching cocktail details', error);},
-
+    this.cocktailcardservice.cocktail$.subscribe(info => {
+        if (info) {
+            this.cocktailFacts = info;
         }
+    })
 
-      );
+    // // grabs the id from the url this.route refers to the injected dependency in the constructor, .snapshot is a property of the dependency that contains a static snapshot of the route at the time of the request .paramMap is an object that provides access to route paratmesters. they are defined in the route with a colon: an example is /cocktial/:id. finally the .get gets the parameters from paramMap with the given variable in the call in this case id
+    // const cocktailId = this.route.snapshot.paramMap.get('id');
 
-    }
+    // // an if statement saying if there is a cocktailId then we do this
+    // if(cocktailId) {
+    //   // this calls our cocktail card servcice to get the cocktails by the id we snagged early and subsribes to the obervable of the service
+    //   this.cocktailcardservice.getCocktailDetailsById(cocktailId).subscribe(
+    //     {
+    //       // next means it happens if the api responds sucessfully and it uses the ternary operator to set cocktail = to the response and = to null if there are no drinks so the app doesn't crash
+    //       next: (response) => {this.cocktail = response.drinks ? response.drinks[0]: null;
+
+    //         // So basically if there is a response from the api it calls the get ingredients function
+    //         if (this.cocktail) {
+    //         this.getIngredientsList();
+    //         }
+    //       },
+    //       // error is what displays if the observable or api fails
+    //       error: (error) => {console.error('Error fetching cocktail details', error);},
+
+    //     }
+
+    //   );
+
+    // }
   }
 
 
-// we need to add logic to listen to search updates 
+// we need to add logic to listen to search updates
 
   getIngredientsList() {
     // Okay so this creates an array of objects {}[] in typescript we have to declare the types of the values coming from the object which is an ingredient: with a string data type and a measure: also with a string data type.
--- src/app/services/cocktail-card.service.ts
+++ src/app/services/cocktail-card.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@angular/core';
 // This one makes it really easy to use http requests you basically just use this.http
 import { HttpClient } from '@angular/common/http';
 // This we've used a ton of time it creates an observable for pushing and pulling data
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+// interface for observable
+import { Drinks, CocktailFacts } from '../models/cocktailCard.model';
+
 
 // the injectable decorator allows dependency injection the provided in 'root' makes it available globally
 @Injectable({
@@ -17,11 +20,26 @@ export class CocktailCardService {
     // injects the http client and gives it the refrence name http
     constructor(private http: HttpClient){}
 
+    // we need a behavior subject to listen to
+    private cocktailInfo$ = new BehaviorSubject<CocktailFacts | null>(null);
+
+    // obeservable for component to listen to
+    cocktail$ = this.cocktailInfo$.asObservable();
+
+
     // creates a function that takes a cocktail id in the form of a string as an argument and
-    getCocktailDetailsById(cocktailId: string | null = null): Observable<any> {
+    getCocktailDetailsById(cocktailId: string | null = null): void {
         // So what this does is create a get http request that accepts any type of response. this.http refers the http module that we injected into this service. its also important to remember that this.http.get returns an observable which is essentially a stream of data
-        console.log('working');
-        return this.http.get<any>(`${this.baseUrl}/lookup.php?i=${cocktailId}`);
+        // console.log('working');
+
+        this.http.get<Drinks>(`${this.baseUrl}/lookup.php?i=${cocktailId}`).pipe(
+            map(response => {
+                const cocktail = response.drinks[0];
+                this.cocktailInfo$.next(cocktail);
+            })
+        )
+
+        // return this.http.get<any>(`${this.baseUrl}/lookup.php?i=${cocktailId}`);
     }
 
 }
