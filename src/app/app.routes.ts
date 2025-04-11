e7abeca0f5c61bbcf95e9aab480dadcf466e0dac
Taylor Kaser <kasertaylor@gmail.com>
2025-04-10 17:11:50 -0700

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CocktailDetailComponent } from './components/cocktail-detail/cocktail-detail.component';
-
-export const routes: Routes = [
-    // So here we add a route so angular knows to send url with cocktail/ an id to the cocktail detail compontent: see cocktial detail component and ing and cocktail comp for more notes 
-    {path: 'cocktail/:id', component: CocktailDetailComponent}
-];
